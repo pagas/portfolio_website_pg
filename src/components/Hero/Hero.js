4c1a21d41d5995ee30b7f42e220438539c653e29
paulius <paulius@2simple.com>
2023-03-12 13:34:05 +0000

    implement hero component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,19 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+        <SectionTitle main center>
+            Welcome To <br />
+            My Personal Portfolio
+        </SectionTitle>
+
+        <SectionText>
+            styled-components is the result of wondering how we could enhance CSS for styling React component systems.
+        </SectionText>
+        <Button onClick={() => window.location = 'https://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
